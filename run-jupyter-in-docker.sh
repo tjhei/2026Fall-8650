97e9822ecbd5459ae2811020f4f20fba68574e95
Timo Heister <timo.heister@gmail.com>
2026-08-21 12:00:41 -0400

    update script again

--- run-jupyter-in-docker.sh
+++ run-jupyter-in-docker.sh
@@ -1,2 +1,2 @@
 #/bin/sh
-docker run --rm -p 8888:8888 -user "$(id -u):$(id -g)" --name jupyter -v "$(pwd):/home/jovyan/work" tjhei/scipy-notebook
+docker run --rm -p 8888:8888 --user root -e CHOWN_HOME=yes -e CHOWN_HOME_OPTS='-R' --name jupyter -v "$(pwd):/home/jovyan/work" tjhei/scipy-notebook
